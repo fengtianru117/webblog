9db10e7251dafca4fe4e93e9523a1fc66ffe827b
fengtianru <fengtianru117@163.com>
2018-05-18 11:33:29 +0800

    ff

--- blog/views.py
+++ blog/views.py
@@ -46,6 +46,20 @@ class ArticleDetailView(DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'article'
 
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(ArticleDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
 
 class CategoryView(ListView):
     model = Article
