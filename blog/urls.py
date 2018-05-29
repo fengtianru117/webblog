0de444716bc3835bb7f226305bdd2e6ad2e12549
fengtianru <fengtianru117@163.com>
2018-05-30 01:07:24 +0800

    'testBatch'

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     re_path(r'^article/(?P<pk>[0-9]+)/$', views.ArticleDetailView.as_view(), name='detail'),
     re_path(r'^article/category/(?P<category_pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
     # re_path(r'^categories/$', views.category, name='category'),
-    #re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
