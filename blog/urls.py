4290577eca5c4f46795575239e97f00b96b2f46b
fengtianru <fengtianru117@163.com>
2018-05-30 01:04:40 +0800

    'testBatch'

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import re_path
 from . import views
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blog'
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     re_path(r'^article/(?P<pk>[0-9]+)/$', views.ArticleDetailView.as_view(), name='detail'),
     re_path(r'^article/category/(?P<category_pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
     # re_path(r'^categories/$', views.category, name='category'),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    #re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
