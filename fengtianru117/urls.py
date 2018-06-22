9573adb645603b4d3c622f6cebbcf7db95e1e5b6
fengtianru <fengtianru117@163.com>
2018-06-22 22:57:24 +0800

    remove xadmin

--- fengtianru117/urls.py
+++ fengtianru117/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-import xadmin
+#import xadmin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
